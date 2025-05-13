8f6af4b06291d0b2e5ce07b248bc8cb141390dd9
kevreco <6670943+kevreco@users.noreply.github.com>
2025-05-13 12:18:55 +0200

    Version: Update app version to 0.0.6

--- src/app/app.hpp
+++ src/app/app.hpp
@@ -4,8 +4,8 @@
 #define TD_APP_NAME "Tendril"
 
 /* Version of the application. */
-#define TD_APP_VERSION_NUMBER (5)
-#define TD_APP_VERSION_TEXT "0.0.5-dev"
+#define TD_APP_VERSION_NUMBER (6)
+#define TD_APP_VERSION_TEXT "0.0.6-dev"
 
 // Prevent windows to add the min/max macro
 #define NOMINMAX
